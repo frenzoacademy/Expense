478898ebcac682fde666964c2fb39975495d6dad
Bhuvibhuvanesh <bhuvaneshwaran23081999@gmail.com>
2024-04-11 18:45:52 +0530

    company updated

---
+++ src/main/java/com/expenses/ExpenseTracker/company/CompanyInterface.java
@@ -0,0 +1,7 @@
+package com.expenses.ExpenseTracker.company;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompanyInterface extends CrudRepository<Company, Integer>{
+
+}
---
+++ src/main/java/com/expenses/ExpenseTracker/company/CompanyService.java
@@ -0,0 +1,32 @@
+package com.expenses.ExpenseTracker.company;
+
+import java.util.List;
+
+public class CompanyService {
+
+	public void addCompany(Company company) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Company> getCompanies() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Company getCompanyById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Company updateCompany(Long id, Company company) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deleteCompany(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
